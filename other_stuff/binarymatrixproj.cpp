6908526a1ea7a6b48376cd02835155e0ac7d9e3c
ColtenMikulastik <mikulastikc@gmail.com>
2023-02-08 13:24:58 +0600

    now functional recursive search, prints node vertexs

--- other_stuff/binarymatrixproj.cpp
+++ other_stuff/binarymatrixproj.cpp
@@ -7,9 +7,9 @@ const int MAT_HEI{ 3 };
 //this is the important matrix
 bool two_d_mat[MAT_HEI][MAT_WID] =
 {
-	{1, 0, 1},
-	{1, 0, 1},
-	{0, 0, 0}
+	{1, 1, 1},
+	{0, 1, 1},
+	{1, 1, 1}
 };
 
 // create 2d array that has all index's 0'd
@@ -20,7 +20,7 @@ bool checked_indexs[MAT_HEI][MAT_WID] = {};
 bool rec_search(int x, int y)
 {
 	// look for 1's that aren't out of bounds and haven't been seen yet
-	if (two_d_mat[x][y] == 1 && x < MAT_WID && y < MAT_HEI && (checked_indexs[x][y] == 0))
+	if (two_d_mat[x][y] == 1 && x < MAT_HEI && y < MAT_WID && x >= 0 && y >= 0 && (checked_indexs[x][y] == 0))
 	{
 		// add index to found index
 		checked_indexs[x][y] = 1;
@@ -28,35 +28,34 @@ bool rec_search(int x, int y)
 		// check indexs all around the current index
 		if (rec_search(x + 1, y))
 		{
-			std::cout << "hello from x + 1";
-			return 1;
+			std::cout << "matrix at node [" << x << "][" << y << "]: and node [" << x + 1 << "][" << y << "]: are connected" << std::endl;
 		}
 		if (rec_search(x - 1, y))
 		{
-			std::cout << "hello from x - 1";
-			return 1;
+			std::cout << "matrix at node [" << x << "][" << y << "]: and node [" << x - 1 << "][" << y << "]: are connected" << std::endl;
 		}
 		if (rec_search(x, y + 1))
 		{
-			std::cout << "hello from y + 1";
-			return 1;
+			std::cout << "matrix at node [" << x << "][" << y << "]: and node [" << x << "][" << y + 1 << "]: are connected" << std::endl;
 		}
 		if (rec_search(x, y - 1))
 		{
-			std::cout << "hello from y - 1";
-			return 1;
+			std::cout << "matrix at node [" << x << "][" << y << "]: and node [" << x << "][" << y - 1 << "]: are connected" << std::endl;
 		}
-		else
-			return 1;
+
+		// once all have been checked, then return 1	
+		return 1;	
+		
 	}
-	else return 0;
+	else 
+		return 0;
 }
 
 
 int main()
 {
 	
-
+	// print the 2d-matrix
 	for (int i{}; i <= (MAT_WID - 1); i++)
 	{
 		for (int j{}; j <= (MAT_HEI - 1); j++)
@@ -65,7 +64,7 @@ int main()
 		}
 		std::cout << std::endl;
 	}
-
+	
 	/*
 	I'm going to make a recusive function...
 	It will look for a 1,
@@ -77,4 +76,14 @@ int main()
 
 	//call function
 	rec_search(0, 0);
+
+	// print the checked_index matrix
+	for (int i{}; i <= (MAT_WID - 1); i++)
+	{
+		for (int j{}; j <= (MAT_HEI - 1); j++)
+		{
+			std::cout << checked_indexs[i][j] << ", ";
+		}
+		std::cout << std::endl;
+	}
 }
