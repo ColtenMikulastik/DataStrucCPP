87e86826e0478f466e5713aeeec6f2c0746c4dd5
Colten <mikulastikc@gmail.com>
2023-03-19 16:24:47 -0500

    making fun power function

--- other_stuff/chapterthree.cpp
+++ other_stuff/chapterthree.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
+int mypower(int num, int pow)
+{
+    int result{num};
+
+    while(pow > 1)
+    {
+        result = result * num;
+        pow--;
+    }
+
+    return result;
+}
+
+
 void rectangle()
 {
+    // this program takes user input and calculates the area of a rectangle
     int length{}, width{};
     std::cout << "let's calculate the area of a triangle" << std::endl;
     std::cout << "length: ";
@@ -15,6 +30,8 @@ void rectangle()
 
 int main()
 {
-    rectangle();
+    // rectangle();
+    std::cout << mypower(12, 4) << std::endl;
+
     return 0;
 }
