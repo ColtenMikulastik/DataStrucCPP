c14a728bab466098a6f103f4fb55d9d48ae84a09
Colten <mikulastikc@gmail.com>
2023-05-05 20:14:39 -0500

    added some comments explaining what some of the method functions do

--- NodeTemplate/NodeTemplate.cpp
+++ NodeTemplate/NodeTemplate.cpp
@@ -16,25 +16,33 @@ public:
         data = value;
         numChildren = 0;
         capacity = initialCapacity;
+        // creates a array of pointers on the heap
         children = new NodeTemplate<T>*[capacity];
     }
 
-    // destructor - recursively deletes all of its child nodes
+    // destructor - deletes all of its child nodes, and array that holds them
     ~NodeTemplate()
     {
+        // loops through the array to delete nodes in the indexs
         for(int i = 0; i < numChildren; i++){
             delete children[i];
         }
+        // deletes the array itself
         delete[] children;
     }
 
     // adding a child node to the array of children, automatically resizing the array if needed.
     void addChild(NodeTemplate<T>* child)
     {
+        // are the number of connected nodes at capacity
         if(numChildren == capacity)
         {
+            // multipying capacity by two
             capacity *= 2;
+            // creating a new array to store pointers
             NodeTemplate<T>** temp = new NodeTemplate<T>*[capacity];
+            // loop through the array, and deep copy pointers
+            // (don't delete old ones so that they don't become unallocated)
             for(int i = 0; i < numChildren; i++)
             {
                 temp[i] = children[i];
@@ -42,6 +50,7 @@ public:
             delete[] children;
             children = temp;
         }
+        // add argument node
         children[numChildren] = child;
         numChildren++;
     }
