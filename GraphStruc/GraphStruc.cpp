a21a4c7220d6ce47c1910ad4a611475b2a226c22
ColtenMikulastik <mikulastikc@gmail.com>
2023-08-09 08:38:00 -0500

    working on creating a graphthing

---
+++ GraphStruc/GraphStruc.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+/* 
+ * the plan is that we have a graph with nodes
+ * the nodes need to be connected with pointers to other nodes
+ * then we need to keep a record of what nodes are connected to each other
+ */
+
+
+class GraphStruc;
+{
+	private:
+		NodeTemplate* nodes;
+		int node_count;
+		// this might need to be just a const size for now
+		bool adj_matrix [][];
+
+	public:
+
+
+
+}
+
+
