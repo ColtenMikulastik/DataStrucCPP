794b7d1c26dad1679a4aff165c916d5d57819ada
Colten Mikulastik <mikulastikc@gmail.com>
2024-03-03 23:27:06 -0600

    Added binary search tree recursive destructor

---
+++ BinaryTree/BinarySearchTree.cpp
@@ -0,0 +1,39 @@
+#include "BinaryNode.cpp"
+
+class BinarySearchTree
+{
+	private:
+		BinaryNode *root;
+		void recersive_delete(BinaryNode *);
+	public:
+		BinarySearchTree(int);
+		void append_node(int);
+		~BinarySearchTree();
+};
+
+BinarySearchTree::BinarySearchTree(int t_value)
+{
+	this->root = new BinaryNode(t_value);
+}
+
+BinarySearchTree::~BinarySearchTree()
+{
+	BinaryNode *start = this->root;
+	
+	// recersively search for leaf
+	recersive_delete(start);
+}
+
+void BinarySearchTree::recersive_delete(BinaryNode *leaf)
+{
+	// search down each leg calling function on each non empty leg
+	if(leaf->get_child_one() != nullptr) { recersive_delete(leaf->get_child_one()); }
+	else if(leaf->get_child_two() != nullptr) { recersive_delete(leaf->get_child_two()); }
+	delete leaf;
+}
+
+void BinarySearchTree::append_node(int t_value)
+{
+	BinaryNode *start = this->root;
+	
+}
