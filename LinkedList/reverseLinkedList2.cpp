4145c5a91bf7b2bc386b2d319b2ee2f087fe5b5d
Colten Mikulastik <mikulastikc@gmail.com>
2024-08-20 16:52:35 -0500

    code style and commenting reversing linkedlists functions

--- LinkedList/reverseLinkedList2.cpp
+++ LinkedList/reverseLinkedList2.cpp
@@ -10,14 +10,19 @@ struct ListNode {
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        
         ListNode *prePtr = NULL, *currPtr = head, *nxtPtr;
+        // loop through list normally, reversing the next of the current ptr
         while(currPtr){
+            // start putting nxt ahead
             nxtPtr = currPtr->next;
+            // assign our currptr's next behind us
             currPtr->next = prePtr;
+            // move the preptr up
             prePtr = currPtr;
+            // move the currptr up
             currPtr = nxtPtr;
         }
+        // preptr should point to the last existing node
         return prePtr;
     }
 };
