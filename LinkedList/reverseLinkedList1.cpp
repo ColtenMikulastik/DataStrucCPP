e3123087a27474474dbfa75fd1ba41f35850b9d7
Colten Mikulastik <mikulastikc@gmail.com>
2024-08-20 17:04:56 -0500

    attempted to apply code style and comments to code

--- LinkedList/reverseLinkedList1.cpp
+++ LinkedList/reverseLinkedList1.cpp
@@ -1,20 +1,28 @@
-struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode(int x){
-          val = x;
-          next = NULL;
-     }
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x)
+    {
+        val = x;
+        next = NULL;
+    }
 };
 
-class Solution {
+class Solution
+{
 public:
-    ListNode* reverseList(ListNode* head) {
-        
+    // recursive function
+    ListNode* reverseList(ListNode* head)
+    {
+        // skip until find null ptr
         if(!head || !head->next) return head;
         
+        // call recursively on head's next opening until end of list
         ListNode* revHead = reverseList(head->next);
+        // begins on end of list, points to last known node for call of function
         ListNode* lastNode = head->next;
+        // recursion is so illegible
         head->next = NULL;
         lastNode->next = head;
         
